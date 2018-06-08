5ddd12dfd820c18f7588ff6ee6357c440edf623e
oliverhuynh <oliver@jufist.com>
2018-06-08 13:14:41 +0700

    toggle cleanup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # toggle
+
+FORMAT
+<a href="#" id="toggle-links" class="toggle-links-1 toggle-links button" data-dropdown=".links">+</a>
+<div class="links"><ul class="links inline">
+  <li class="node_update_detail"><a href="#" title="Update Attribute">Update Attribute</a></li>
+  <li class="node_delete"><a href="#" title="Delete content">Delete content</a></li>
+</ul></div>
--- toggle.js
+++ toggle.js
@@ -1,5 +1,5 @@
 (function($, $GLOBAL) {
-var toggleNode = {
+$.jftoggle = {
   toggle: function($elm, state) {
     if (state == $elm.hasClass('active')) {
       return ;
@@ -18,15 +18,38 @@ var toggleNode = {
   toggleClick: $.debounce(1000,true, function(e) {
     e.preventDefault();
     var $elm = $(this).closest('td');
-    toggleNode.toggle($elm, false);
+    $.jftoggle.toggle($elm, false);
   }),
-  activeDropDown: $.debounce(1000,true, function(e) {
-    e.preventDefault();
-    $(this).siblings('.links').toggle();
-    $(this).closest('tr').toggleClass('tr-active-dropdown');
-  }),
+  options: {},
+  activeDropDown: function() {
+    this.$dropdown.toggle();
+    this.options.$elm.closest('tr').toggleClass('tr-active-dropdown');
+  },
+  create: function(options) {
+    this.options = options;
+    var it = $.extend(true, {}, this);
+    it.attachItem();
+    return it;
+  },
+  $dropdown: false,
+  attachItem: function() {
+    var $elm = this.options.$elm;
+    this.$dropdown = $elm.siblings($elm.attr("data-dropdown"));
+    this.$dropdown.addClass("toggle-links-dropdown");
+    $elm.add(this.$dropdown).wrapAll('<span class="toggle-links-dropdown-wrapper" />');
+    var t = this;
+    this.options.$elm.click(function(e) {
+      e.preventDefault();
+      t.activeDropDown();
+    });
+  },
   attach: function(context){
-    var toggle = toggleNode.toggle;
+    $('.toggle-links-1').once('ltoggle4').each(function() {
+      $.jftoggle.create({
+        $elm: $(this)
+      });
+    });
+    var toggle = $.jftoggle.toggle;
     $('.view-og-ghp-ron .view-content .node .link, .view-board .view-content .node a.link, a.togglelink', context).once('ltoggle').click(function(e){
       e.preventDefault();
       var $elm = $(this).closest('td');
@@ -36,12 +59,7 @@ var toggleNode = {
       var $links = $(this).closest('.node').find('div.links ul.links');
       $links.append('<li><a href="' + $(this).attr('href') + '">'+ Drupal.t('View full') +'</a></li>');
     });
-    $(".views-field-nodefield div.links").once('ltoggle3').before('<a href="#" id="toggle-links-2" class="toggle-links-2 toggle-links button ui-link"><i class="fa fa-toggle-off"></i></a><a href="#" id="toggle-links" class="toggle-links-1 toggle-links button">+</a>');
-    $('.toggle-links-1').once('ltoggle4').click(toggleNode.activeDropDown);
-    $('.toggle-links-2').once('ltoggle5').click(toggleNode.toggleClick);
+    $('.toggle-links-2').once('ltoggle5').click(this.toggleClick);
   }
 };
-
-
-Drupal.behaviors.toggleNode = toggleNode.attach;
 })(jQuery, jQueryLatest);
